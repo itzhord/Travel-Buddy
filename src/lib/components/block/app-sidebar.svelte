9dbe9bf800c5bc1de9b23e7d5060b2edfbae80b6
itzhord <mhiztafranck@gmail.com>
2025-03-17 19:42:57 +0100

    style: update sidebar styles and improve layout with new variables and icons

--- src/lib/components/block/app-sidebar.svelte
+++ src/lib/components/block/app-sidebar.svelte
@@ -5,6 +5,7 @@
     import Search from "@lucide/svelte/icons/search";
     import Settings from "@lucide/svelte/icons/settings";
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import Buddy from '$lib/components/assets/buddy logo.svg'
     
     // Menu items.
     const items = [
@@ -14,24 +15,19 @@
       icon: House,
      },
      {
-      title: "Inbox",
+      title: "Message",
       url: "#",
       icon: Inbox,
      },
      {
-      title: "Calendar",
+      title: "Notification",
       url: "#",
       icon: Calendar,
      },
      {
-      title: "Search",
+      title: "Setting",
       url: "#",
       icon: Search,
-     },
-     {
-      title: "Settings",
-      url: "#",
-      icon: Settings,
      },
     ];
    </script>
@@ -39,16 +35,16 @@
    <Sidebar.Root>
     <Sidebar.Content>
      <Sidebar.Group>
-      <Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+      <Sidebar.GroupLabel class="my-8"><img src={Buddy} alt=""/></Sidebar.GroupLabel>
       <Sidebar.GroupContent>
        <Sidebar.Menu>
         {#each items as item (item.title)}
          <Sidebar.MenuItem>
           <Sidebar.MenuButton>
            {#snippet child({ props })}
-            <a href={item.url} {...props}>
-             <item.icon />
-             <span>{item.title}</span>
+            <a href={item.url} {...props} class="text-[1.3rem] flex my-3 items-center space-x-4 ml-4"> 
+             <item.icon/>
+             <span >{item.title}</span>
             </a>
            {/snippet}
           </Sidebar.MenuButton>
